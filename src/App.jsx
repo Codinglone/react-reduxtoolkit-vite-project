101c7e7c361b55c5f17f01644913a0dbe549d48f
Fabrice <codinglone@gmail.com>
2023-04-21 21:45:09 +0200

    Fixed the bug

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import { increment, decrement, incrementByAmount, selectCount } from "./features
 function App() {
   const count = useSelector(selectCount)
   const dispatch = useDispatch()
-  const [incrementAmount, setIncrementAmount] = useState('2')
+  const [incrementAmount, setIncrementAmount] = useState(0)
+  const incrementValue = Number(incrementAmount) || 0;
   return (
     <>
       <h1>{count}</h1>
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <input type="number" onChange={(e) => setIncrementAmount(e.target.value)} />
+      <button onClick={() => dispatch(incrementByAmount(incrementValue))}>Increment by amount</button>
     </>
   )
 }
